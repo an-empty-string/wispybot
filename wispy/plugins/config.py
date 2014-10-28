f761840806d5c113a51a06e6f518325cbf80b974
fwilson <fwilson@fwilson.me>
2014-10-28 17:29:14 -0400

    maybe add get_config

--- wispy/plugins/config.py
+++ wispy/plugins/config.py
@@ -11,5 +11,16 @@ def set_config(reply, user, typeargs, args, conn, event):
         reply("Failure!")
     reply("Success!")
 
+def get_config(reply, user, typeargs, args, conn, event):
+    key = typeargs[0].split(".")
+    conf = conn.config
+    try:
+        for i in key[:-1]:
+            conf = conf[i]
+        reply(conf[key[-1]])
+    except:
+        reply("Nonexistent configuration key")
+
 def register_callbacks(conn):
-    conn.register_command("config_set", set_config, str, str, send_extra_info=True, permission="admin")
+    conn.register_command("config set", set_config, str, str, send_extra_info=True, permission="admin")
+    conn.register_command("config get", get_config, str, send_extra_info=True, permission="admin")
