779b5455eb1158458a0d79716b7b0fe92bcbb77a
fwilson <fwilson@fwilson.me>
2014-11-14 20:09:38 -0500

    config file path

--- bot.py
+++ bot.py
@@ -4,5 +4,5 @@ import wispy.config
 import wispy.core
 
 if __name__ == '__main__':
-    config = wispy.config.read_from_file(sys.argv[1])
+    config = wispy.config.read_from_file("config.yml")
     wispy.core.start(config)
